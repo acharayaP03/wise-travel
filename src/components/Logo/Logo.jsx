c62d5da32eb8010e37404352326518c68f385eda
acharayaP03 <acharyaP03@gmail.com>
2024-06-23 08:57:05 +1000

    need to fix logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
-import styles from "./Logo.module.css";
-import {Link} from "react-router-dom";
+import styles from './Logo.module.css';
+import { Link } from 'react-router-dom';
 
 function Logo() {
-  return (
-          <Link to="/">
-            <img src="/public/logo.png" alt="WorldWise logo" className={styles.logo} />
-          </Link>
-      )
-
+	return (
+		<Link to='/'>
+			{/* <img src="/public/logo.png" alt="WorldWise logo"  /> */}
+			<h3 className={styles.logo}>Wise Travel</h3>
+		</Link>
+	);
 }
 
 export default Logo;
