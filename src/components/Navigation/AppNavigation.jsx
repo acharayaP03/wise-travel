5c5a75da19f03443d3bc1a5d50b468cf3a0f6536
acharayaP03 <acharyaP03@gmail.com>
2024-06-23 15:50:12 +1000

    traditional route

--- src/components/Navigation/AppNavigation.jsx
+++ src/components/Navigation/AppNavigation.jsx
@@ -1,5 +1,21 @@
-// import styles from './AppNav.module.css';
+import { NavLink } from 'react-router-dom';
+import styles from './AppNav.module.css';
 // import SideBar from '../SideBar/SideBar.jsx';
 export function AppNavigation() {
-	return <div>app navigation</div>;
+	return (
+		<nav className={styles.nav}>
+			<ul>
+				<li>
+					<NavLink to='cities' exact activeClassName={styles.active}>
+						Cities
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='countries' exact activeClassName={styles.active}>
+						Countries
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 }
