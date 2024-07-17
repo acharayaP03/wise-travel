70595c80d25627ea8624376d93fc21e215018661
acharayaP03 <acharyaP03@gmail.com>
2024-07-18 07:52:19 +1000

    dynamic import with @

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 export default defineConfig({
 	plugins: [react(), eslint()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	server: {
 		port: 3000,
 		proxy: {
