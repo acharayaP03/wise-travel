c741cd85e323b07489f9be9d76be4607b28f5bb2
acharayaP03 <acharyaP03@gmail.com>
2024-08-01 21:09:37 +1000

    authenthicated user and logout

--- client/src/pages/AppLayout/AppLayout.jsx
+++ client/src/pages/AppLayout/AppLayout.jsx
@@ -1,11 +1,13 @@
 import SideBar from '../../components/SideBar/SideBar';
 import styles from './AppLayout.module.css';
 import Map from '../../components/Map/Map';
+import User from '../../components/User/User';
 export default function AppLayout() {
 	return (
 		<div className={styles.app}>
 			<SideBar />
 			<Map />
+			<User />
 		</div>
 	);
 }
